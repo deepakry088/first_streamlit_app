14be9df09e6f76297f34ed66aecd90ff06eba2fb
deepakry088 <139115953+deepakry088@users.noreply.github.com>
2023-07-10 16:46:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('M Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text(' Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
             
